c2ec96c5860a222109eac85d0b6ce17f7086b177
Carlos <carloscuba014@gmail.com>
2025-07-10 15:49:04 -0400

    fetch api url for different envs

--- Program.cs
+++ Program.cs
@@ -6,6 +6,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var apiUrl = builder.Configuration["API_URL"];
+
+builder.Services.AddScoped(sp => new HttpClient
+{
+  BaseAddress = new Uri(apiUrl!)
+});
 
 await builder.Build().RunAsync();
