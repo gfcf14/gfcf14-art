52873be838844ce366dbed5b1507817e3f2a56de
Carlos <gfcf14@gmail.com>
2025-07-11 19:23:14 -0400

    prepare workflow for deployment and dynamic api url load

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Text.Json;
 using gfcf14_art.Services;
 using gfcf14_art;
 
@@ -7,7 +8,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-var apiUrl = builder.Configuration["API_URL"];
+
+using var http = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
+var configJson = await http.GetStringAsync("appsettings.json");
+using var doc = JsonDocument.Parse(configJson);
+var apiUrl = doc.RootElement.GetProperty("API_URL").GetString();
 
 builder.Services.AddScoped(sp => new HttpClient
 {
